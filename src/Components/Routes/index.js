f4cdb24cd83dc4780b09d6e1e3d9bbdb86acacf7
Shubham Gupta <shubhamgupta659@gmail.com>
2023-04-20 12:13:39 +0800

    sync

--- src/Components/Routes/index.js
+++ src/Components/Routes/index.js
@@ -1,23 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../../Pages/Home";
-import Dashboard from "../../Pages/Dashboard";
-import ApplyTaxIncentiveDash from "../../Pages/TaxIncentiveDash";
 import Stage1Dash from "../../Pages/Stage1/dash";
 import Stage1App from "../../Pages/Stage1/fiApp";
 import Stage2Dash from "../../Pages/Stage2/dash";
-import ViewAppAction from "../../Pages/CaseView/view";
 import Stage3Dash from "../../Pages/Stage3/dash";
+import ViewAppAction from "../../Pages/CaseView/view";
 import Login from "../../Pages/Login/login";
-import TIPSHome from "../../Pages/Home";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />}></Route>
       <Route path="/login" element={<Login />}></Route>
-      <Route path="tipslogin" element={<TIPSHome />}></Route>
-      <Route path="/dashboard" element={<Dashboard />}></Route>
-      <Route path="/applyNewIncentive" element={<ApplyTaxIncentiveDash />}></Route>
       <Route path="/stage1" element={<Stage1Dash />}></Route>
       <Route path="/stage2" element={<Stage2Dash />}></Route>
       <Route path="/stage3" element={<Stage3Dash />}></Route>
